ae97bbe75e100a718ba906a51b7520c391b758b9
Daniel Micay <danielmicay@gmail.com>
2017-01-06 00:13:05 -0500

    set up in-tree kernel builds
    Change-Id: I72437e9b8dff9e84f004c8b4260381be3962a93c

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -56,21 +56,12 @@ current_dir := $(notdir $(patsubst %/,%,$(dir $(mkfile_path))))
 ifeq ($(TARGET_KERNEL_VERSION),)
     TARGET_KERNEL_VERSION := 3.18
 endif
-TARGET_KERNEL := msm-$(TARGET_KERNEL_VERSION)
-ifeq ($(TARGET_KERNEL),$(current_dir))
-    # New style, kernel/msm-version
-    BUILD_ROOT_LOC := ../../
-    TARGET_KERNEL_SOURCE := kernel/$(TARGET_KERNEL)
-    KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel/$(TARGET_KERNEL)
-    KERNEL_SYMLINK := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-    KERNEL_USR := $(KERNEL_SYMLINK)/usr
-else
-    # Legacy style, kernel source directly under kernel
-    KERNEL_LEGACY_DIR := true
-    BUILD_ROOT_LOC := ../
-    TARGET_KERNEL_SOURCE := kernel
-    KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-endif
+TARGET_KERNEL := google/marlin
+BUILD_ROOT_LOC := ../../../
+TARGET_KERNEL_SOURCE := kernel/$(TARGET_KERNEL)
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel/$(TARGET_KERNEL)
+KERNEL_SYMLINK := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_USR := $(KERNEL_SYMLINK)/usr
 
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 
@@ -83,7 +74,7 @@ $(info Using uncompressed kernel)
 TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image
 else
 ifeq ($(KERNEL_ARCH),arm64)
-TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.gz
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.lz4
 else
 TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/zImage
 endif
